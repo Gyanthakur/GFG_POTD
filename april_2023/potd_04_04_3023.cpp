49c9c29ea00ae3339c962efcaca1d5c0b75ff16b
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-04-04 09:37:10 +0530

    04_april_2023

---
+++ april_2023/potd_04_04_3023.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//string contains only "a" and "b" many times   like "aabbbabbbabbb", "abbababbababba", "ab"
+int minSteps(string str) {
+    int ans=0;
+    int n=str.size();
+    for(int i=0;i<n;i++)
+    {
+        if(str[i]!=str[i-1])
+        ans++;
+    }
+    return ans/2+1;
+
+}
+
+int main()
+{
+    string s;
+    cin>>s;
+    int ans=minSteps(s);
+    cout<<ans<<endl;
+}
