121d4004dc54cec93b5e3fb07fae85ce6e34acff
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-04-10 16:19:21 +0530

    10_april_2023

---
+++ april_2023/potd_10_04_2023.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+class Solution {
+  public:
+    int maxIntersections(vector<vector<int>> lines, int N) {
+       map<int,int> mp;
+      for(auto it:lines){
+          int start=it[0];
+          int end=it[1]+1;
+          mp[start]++;
+          mp[end]--;
+      }
+       
+       int prefix=0;
+       int ans=1;
+       for(auto it: mp){
+           prefix=prefix+it.second;
+           ans=max(ans,prefix);
+       }
+       return ans;
+    }
+};
