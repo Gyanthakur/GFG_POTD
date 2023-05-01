f19b1c6fb972bedce3c104ce003990a20ce13f03
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-05-01 19:23:31 +0530

    01_may_2023

---
+++ may_2023/potd_01_05_2023.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+
+int duplicateSubtreeNaryTree(Node *root){
+           unordered_map<string, int> subtreeMap;
+        constructSubtreeString(root, subtreeMap);
+        int count = 0;
+        for (auto it : subtreeMap) {
+            if (it.second > 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+private:
+    string constructSubtreeString(Node* node, unordered_map<string, int>& subtreeMap) {
+        string s = to_string(node->data) + ":";
+        vector<string> childSubtrees;
+        for (auto child : node->children) {
+            childSubtrees.push_back(constructSubtreeString(child, subtreeMap));
+        }
+        sort(childSubtrees.begin(), childSubtrees.end());
+        for (auto childSubtree : childSubtrees) {
+            s += childSubtree + ",";
+        }
+        subtreeMap[s]++;
+        return s;
+    }
