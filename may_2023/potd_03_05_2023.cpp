219a96bc8a1cda12c813d322878c230f4e903f9a
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-05-03 16:29:07 +0530

    03_may_2k23

---
+++ may_2023/potd_03_05_2023.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+
+
+class Solution{
+public:
+    bool makePalindrome(int n,vector<string> &arr){
+        map<string,int> mp;
+        for(auto it: arr) mp[it]++;
+        
+        for(auto str: arr){
+            string rev=str;
+            reverse(rev.begin(),rev.end());
+            if(mp.find(rev)==mp.end()){
+                 return false; 
+            }
+            else {
+                if(mp[rev]>1) mp[rev]--;
+                else
+                mp.erase(rev);
+            }
+        }
+        return true;
+    }
+};
