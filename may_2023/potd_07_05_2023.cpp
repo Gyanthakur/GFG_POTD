b25d02e726625c2fae0d1b8552b0ee9185fd0537
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-05-07 18:28:09 +0530

    07_may_2k23

---
+++ may_2023/potd_07_05_2023.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+class Solution{
+public:
+     string stringMirror(string str){
+        string s="",f;
+        s+=str[0];
+        for(int i=1;i<str.size();i++){
+            if(s[i-1]>str[i]){
+                s+=str[i];
+            }
+            else if(s[i-1]==str[i]){
+                if(s.size()==1)break;
+                s+=str[i];
+            }
+            else break;
+        }
+        f+=s;
+        reverse(s.begin(),s.end());
+        f+=s;
+        return f;
+    }
+};
