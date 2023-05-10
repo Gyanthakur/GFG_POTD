88a434886a56b0dd7163c31056c1b7211cc061f2
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-05-10 21:16:41 +0530

    10_may_2k23

---
+++ may_2023/potd_10_05_2023.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+class Solution{
+public:
+    int totalCuts(int N,int K,vector<int> &A){
+        // Code here
+        vector<int> pref(N, 0);
+        pref[N-1] = A[N-1];
+        for(int i = N-2; i>=0 ; i--){
+            pref[i] = min(pref[i+1],A[i]);
+        }
+        int maxi = A[0];
+        int ans = 0;
+        for(int i = 1; i < N; i++){
+            if(maxi + pref[i] >= K)
+                ans++;
+            maxi = max(maxi, A[i]);
+        }
+        return ans;
+    }
+};
