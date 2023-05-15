4d6fb2e6b7db349331d92a6a05648918fa3934b5
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-05-15 18:17:38 +0530

    15_may_2k23

---
+++ may_2023/potd_15_05_2023.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+// class Solution {
+//   public:
+//     // O(n) - Time 
+//     // O(n) - space 
+//     // DP Solution
+//     long long countBits(long long N) {
+//         vector<int> setBitCount(N + 1, 0);
+//         long long res = 0;
+        
+//         for(long long i = 1; i <= N; i++) {
+            
+//             // odd case
+//             if(i & 1) 
+//                 setBitCount[i] = setBitCount[i >> 1] + 1;
+            
+//             // even case
+//             else
+//                 setBitCount[i] = setBitCount[i >> 1];
+            
+//             // storing count
+//             res += setBitCount[i];
+            
+//         }
+        
+//         return res;
+//     }
+// };
+
+class Solution {
+  public:
+    long long countBits(long long N) {
+        long long ans = 0;
+        
+        int a = 1;
+        int b = 1 << a;
+        
+        for(int i = 0; i < 31; i++) {
+            
+            ans += (N / b) * a;
+            int rem = (N % b) - a + 1;
+            
+            if(rem > 0)
+                ans += rem;
+                
+            a <<= 1;
+            b <<= 1;
+        }
+        return ans;
+    }
+};
