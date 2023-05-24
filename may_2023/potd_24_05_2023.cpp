5db8c3b3ff51dcd4fd8dac7f7622727dd4e194cc
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-05-24 16:57:43 +0530

    24_may_2k23

---
+++ may_2023/potd_24_05_2023.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+class Solution {
+  public:
+    int getMaximum(int N, vector<int> &arr) {
+        // code here
+        if(N == 1)
+            return 1;
+        long long sum = 0;
+        for(auto elem: arr)
+            sum += elem;
+        while(N != 0 && sum % N-- != 0);
+        return N + 1;
+    }
+};
