8d3f76d3b40f1384f4733bcc81ef73dd92d5b915
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-05-27 11:09:02 +0530

    27_may_2k23

---
+++ may_2023/potd_27_05_2023.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+class Solution{
+    public:
+    struct Node* modifyTheList(struct Node *head)
+    {
+        //add code here.
+        vector<int> arr;
+        Node* ans = head;
+        Node *temp = head;
+        while(temp!=NULL){
+            arr.push_back(temp->data);
+            temp = temp->next;
+        }
+        int n = arr.size();
+        for(int i=0;i<n/2;i++){
+            ans->data = arr[n-1-i]-arr[i];
+            ans = ans->next;
+        }
+        for(int i=n/2;i<n;i++){
+            ans->data = arr[n-i-1];
+            ans = ans->next;
+        }
+        return head;
+        
+        
+    }
+};
