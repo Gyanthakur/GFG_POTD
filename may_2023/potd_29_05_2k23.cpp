365b1ee934596416ab33d2fa60735b28c995585d
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-05-29 15:42:15 +0530

    29_may_2k23

---
+++ may_2023/potd_29_05_2k23.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+class Solution {
+  public:
+    bool matchPattern(string& word, string& patternToMatch) {
+        string patternInWord = "";
+        
+        for(char c: word) {
+            if(isupper(c)) patternInWord += c;
+            if(patternInWord == patternToMatch) return true;
+        } 
+        
+        return patternInWord == patternToMatch;
+    }
+    
+    vector<string> CamelCase(int n, vector<string> dictionary, string pattern) {
+        // code here
+        vector<string> ans;
+        
+        for(string word: dictionary) {
+            if(matchPattern(word, pattern)) {
+                ans.push_back(word);
+            }
+        }
+        
+        sort(begin(ans), end(ans));
+        
+        if(ans.empty()) ans.push_back("-1");
+        
+        return ans;
+    }
+};
