b488476fdfdf48b35c5ff5ef97d1ba0c6ffe285f
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-05-31 10:08:51 +0530

    31_may_2k23

---
+++ may_2023/potd_31_05_2023.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+class Solution{
+public:
+    int LargButMinFreq(int arr[], int n) {
+        // code here
+        
+        unordered_map<int, int> container;
+        
+        for(int i = 0; i < n; i++) {
+            container[arr[i]]++;
+        }
+        
+        int frequency = INT_MAX;
+        int number = 0;
+        
+        for(auto el : container) {
+            if(el.second < frequency) {
+                number = el.first;
+                frequency = el.second;
+            } else if(el.second == frequency && el.first > number) {
+                number = el.first;
+            }
+        }
+        
+        return number;
+    }
+};
