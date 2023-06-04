950e9897e25226e950933e1a996348f9eaafb592
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-06-04 14:39:01 +0530

    04_june_2k23

---
+++ June_2023/potd_04_06_2k23.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+return 0;
+}
+
+class Solution
+{
+  public:
+    bool isNum(char ch){
+        if(ch != '+' && ch != '-' && ch != '*' && ch != '/') return true;
+        return false;
+    }
+    
+    string reverseEqn (string s)
+        {
+            stack<string> st;
+            string str = "", res = "";
+            
+            for(int i=0;i<s.size();i++){
+                if(isNum(s[i])) str += s[i];
+                else{
+                    res += s[i];
+                    st.push(str);
+                    st.push(res);
+                    str = "";
+                    res = "";
+                }
+            }
+            st.push(str);
+            
+            while(!st.empty()){
+                res += st.top();
+                st.pop();
+            }
+            return res;
+        }
+};
