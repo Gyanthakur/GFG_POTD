2232887b10605e9f09e0375eb8b119318bf0cf86
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-06-05 15:32:54 +0530

    05_june_2k23

---
+++ June_2023/potd+05+06_2023.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+class Solution
+{
+    public:
+
+    int minDiff(Node *root, int K)
+    {
+
+        if(root == NULL) return INT_MAX;
+        if(root->data==K) return 0;
+        if(root->data>K) return min(root->data-K,minDiff(root->left,K));
+        if(root->data<K) return min(K-root->data,minDiff(root->right,K));
+
+    }
+};
