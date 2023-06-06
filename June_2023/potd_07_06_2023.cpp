b91be68413865747fcd744ec7d24ad9ee396d895
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-06-07 02:01:05 +0530

    07_june_2k23

---
+++ June_2023/potd_07_06_2023.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+class Solution {
+  public:
+    vector<int> leastPrimeFactor(int n) {
+        vector<int>v(n+1,0);
+        for(int i=0;i<=n;i++)
+        {
+            v[i]=i;
+        }
+        for(int i=2;i*i<=n;i++)
+        {
+            if(v[i]==i)
+            {
+                for(int j=i;j<=n;j+=i)
+                {
+                    if(v[j]==j)
+                    v[j]=i;
+                }
+            }
+        }
+        return v;
+    }
+};
