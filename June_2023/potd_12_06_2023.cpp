47179c36c58c96f2dbb22e1cb73416435d613cfe
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-06-12 09:20:28 +0530

    12_june_2k23

---
+++ June_2023/potd_12_06_2023.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+class Solution{
+  public:
+    int cutRod(int price[], int n) {
+        //code here
+         vector<int> dp(n+1,0);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                dp[i]=max(dp[i],price[j-1]+dp[i-j]);
+            }
+        }
+        return dp[n];
+    }
+};
