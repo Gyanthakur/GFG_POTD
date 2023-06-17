8f42e217eb8769abed837cf51c98b9b2eedda772
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-06-17 19:25:12 +0530

    17_june_2k23

---
+++ June_2023/potd_17_06_2023.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+class Solution{
+    public:
+    unordered_map<int,int>umap;
+    // Function to insert element into the queue
+    void insert(queue<int> &q, int k){
+        q.push(k);
+        umap[k]++;
+        
+    }
+    
+    // Function to find frequency of an element
+    // return the frequency of k
+    int findFrequency(queue<int> &q, int k){
+        if(umap.find(k)!=umap.end()) return umap[k];
+        else return -1;
+        
+    }
+    
+};
