dd31b5962b21b82de32238db14875b7d08216d2a
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-06-19 18:48:17 +0530

    19_june_2k23

---
+++ June_2023/potd_19_06_2023.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to rearrange an array so that arr[i] becomes arr[arr[i]]
+    //with O(1) extra space.
+    void arrange(long long arr[], int n) {
+        for(int i = 0; i<n; i++) {
+            arr[i] = arr[i] + (arr[arr[i]]%n)*n;
+        }
+        for(int i = 0; i<n; i++) {
+            arr[i] = arr[i]/n;
+        }
+        return;
+    }
+};
