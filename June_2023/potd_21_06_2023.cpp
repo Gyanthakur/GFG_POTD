bbbb134080c2675924d637eaa2be7b27ff54a914
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-06-21 13:06:42 +0530

    21_june_2k23

---
+++ June_2023/potd_21_06_2023.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+class Solution {
+  public:
+    int sumOfNaturals(int n) {
+        // code here
+           long long ans=0;
+        long long temp = (pow(10,9)+7);
+       ans = (((n%temp)*((n+1)%temp))/2)%temp;
+        return ans%temp;
+            
+    }
+};
