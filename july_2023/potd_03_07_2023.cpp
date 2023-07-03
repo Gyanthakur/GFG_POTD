85256886ec24a04fd8599e120a4f58896cd1ba05
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-07-03 17:31:38 +0530

    03_july_2023

---
+++ july_2023/potd_03_07_2023.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+class Solution{
+public:
+    int maxIndexDiff(int arr[], int n) {
+        // code here
+        int nums[n];
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=arr[i];
+        }
+        sort(nums,nums+n);
+        int k=n-1;
+        int ans=0;
+        int i=0;
+        int j=n-1;
+        while(i<j)
+        {
+            int diff=j-i;
+            if(arr[i]<=arr[j])
+            {
+                ans=max(ans,diff);
+                i++;
+                j=n-1;
+            }
+            else if(arr[i]==nums[k])
+            {
+                k--;
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        return ans;
+    }
+
+};
