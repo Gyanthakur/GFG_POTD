8c98d33c87a026852e40117ed86a0c9a82d241d6
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-07-08 21:51:40 +0530

    08_july_2k23

---
+++ july_2023/potd_08_07_2023.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+class Solution{
+  public:
+    //Function to find triplets with zero sum.
+    bool findTriplets(int arr[], int n)
+    {  
+        //Your code here
+        sort(arr,arr+n);
+        for(int i=0;i<n;i++){
+              int j=i+1;
+              int k=n-1;
+              while(j<k){
+                  if(arr[i]+arr[j]+arr[k]==0){
+                      return true;
+                  }else if(arr[i]+arr[j]+arr[k]<0){
+                      j++;
+                  }else{
+                      k--;
+                  }
+              }
+        }
+        return false;
+    }
+};
