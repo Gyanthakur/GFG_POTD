9bf05c75717df65dbe508d601022f049b0e22162
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-07-14 10:44:53 +0530

    14_july_2k23

---
+++ july_2023/potd_14_07_2023.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+class twoStacks
+{
+    int *arr;
+    int size;
+    int top1, top2;
+    public:
+    
+    twoStacks(int n=100)
+    {
+        size = n; 
+        arr = new int[n]; 
+        top1 = -1; 
+        top2 = size;
+    }
+ 
+    //Function to push an integer into the stack1.
+    void push1(int x)
+    {
+        arr[++top1] = x;
+    }
+    
+    //Function to push an integer into the stack2.
+    void push2(int x)
+    {
+       arr[--top2] = x;
+    }
+    
+    //Function to remove an element from top of the stack1.
+    int pop1()
+    {
+        
+        if(top1 == -1)   return top1;
+        
+        int topElem1 = arr[top1--];
+        
+        return topElem1;
+    }
+    
+    //Function to remove an element from top of the stack2.
+    int pop2()
+    {
+        if(top2 == size)   return -1;
+        
+        int topElem2 = arr[top2++];
+        
+        return topElem2;
+    }
+};
