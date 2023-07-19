4230df1729dd86994c3d88ae7135a5d96aaa0076
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-07-19 08:19:50 +0530

    19_july_2k23

---
+++ july_2023/potd_19_07_2023.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+class Solution{
+  public:
+    int longestPalinSubseq(string A) {
+          int n=A.size();
+          string B = A;
+          reverse(B.begin(),B.end());
+          vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+          
+          for(int i=1;i<=n;i++){
+              for(int j=1;j<=n;j++){
+                  if(A[i-1]==B[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                  else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+              }
+          }
+          
+          return dp[n][n];
+    }
+};
