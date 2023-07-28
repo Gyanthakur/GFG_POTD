fd66d07c51ae7ddf2eec597b091fc4c64fe6c6c6
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-07-28 12:24:13 +0530

    28_july_2023

---
+++ july_2023/potd_28_07_2023.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+class Solution{
+    public:
+        Node* LCA(Node *root, int n1, int n2)
+        {
+            if(!root) return root;
+            if(n1 < root->data and n2 < root->data) return LCA(root->left,n1,n2);
+            if(n1 > root->data and n2 > root->data) return LCA(root->right,n1,n2);
+            return root;
+        }
+
+ 
+};
