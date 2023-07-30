ea67cc5bc1bcfe35a67f87835552e222623968d7
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-07-30 13:42:35 +0530

    30_july_2k23

--- july_2023/potd_30_07_2k23.cpp
+++ july_2023/potd_30_07_2k23.cpp
@@ -4,3 +4,33 @@ int main(){
 
 return 0;
 }
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution{
+  public: 
+    Node * inOrderSuccessor(Node *root, Node *x){
+        //Your code here
+        Node *successor = NULL;         //1
+        while(root != NULL)                  //2
+        {
+            if(x->data >= root->data){    //3
+                root = root->right;    //4
+            }
+            else{                       //5
+                successor = root;   //6
+                root = root->left;   //7
+            }
+        }
+        return successor;
+    }
+};
