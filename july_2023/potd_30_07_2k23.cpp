2ed9c98db0c85afcdba513afd70f56299ae7067e
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-07-31 18:58:09 +0530

    31_july_2k23

--- july_2023/potd_30_07_2k23.cpp
+++ july_2023/potd_30_07_2k23.cpp
@@ -14,7 +14,6 @@ struct Node {
         left = right = NULL;
     }
 };
-*/
 
 class Solution{
   public: 
---
+++ july_2023/potd_31_07_2023.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        
+        // first make container and visited.
+        vector<int> bfs;
+        vector<int> visit(V,0);
+        
+        // second make Queue and push zero in queue first
+        queue<int> q;
+        q.push(0);
+        // also make visited.
+        visit[0] = 1;
+        
+        // make loop and run until queue run out.
+        while(!q.empty()){
+            // take first element in queue stored and make queue empty.
+            int value = q.front();
+            q.pop();
+            
+            // take this value in bfs container
+            bfs.push_back(value);
+            
+            // check in loop value is visited or not
+            for(auto it : adj[value]){
+                // if value is not visited then visit this value and push in queue also
+                if(!visit[it]){
+                    q.push(it);
+                    visit[it] = 1;
+                }
+            }
+        }
+        return bfs;
+    }
+
+ 
+};
