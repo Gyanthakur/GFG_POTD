6d7c6ff6aebd8514e8a3e6e0d64b0b8567dd05e2
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-09-14 17:10:17 +0530

    14_september_2k23

---
+++ september_2023/potd_14_09_2023.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+long long  t, a[100005];
+int mod = 1e9+7;
+    int f(int i, int n, int arr[], int sum, vector<vector<int>>& dp){
+        if(i == n){
+            if(sum == 0)
+                return 1;
+            else
+                return 0;
+        }
+        
+       // if(sum == 0)
+       //     return 1;
+            
+        if(dp[i][sum] != -1)
+            return dp[i][sum];
+        int pick = 0, notPick;
+        
+        if(arr[i] <= sum){
+            pick = f(i+1, n, arr, sum-arr[i], dp);
+        }
+        
+        notPick = f(i+1, n, arr, sum, dp);
+        
+        return dp[i][sum] = (pick%mod + notPick%mod)%mod;
+        
+    }
+    int perfectSum(int arr[], int n, int sum)
+    {
+        // Your code goes here
+        vector<vector<int>> dp(n, vector<int>(sum+1, -1));
+        return f(0, n, arr, sum, dp);
+    }
+int32_t main()
+{
+ios::sync_with_stdio(0);
+cin.tie(0);
+
+return 0;
+}
