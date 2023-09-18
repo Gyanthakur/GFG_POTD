fbf3bf1ac40829ba4f521acc2a13935982695691
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-09-18 21:08:10 +0530

    18_september_2k23

---
+++ september_2023/potd_18_09_2023.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+#include<map>
+#include<math.h>
+#include<set>
+using namespace std;
+#define int long long
+long long  t;
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        // Your code here  
+        // if(n%2==0)return "YES";
+        // else return "NO";
+        // return (n>0 && (n&(n-1)) == 0)? true : false;
+        return n ? n == pow(2,(int)log2(n)) : 0;
+        
+    }
+};
+void solution()
+{
+
+}
+int32_t main()
+{
+ios_base::sync_with_stdio(false); 
+cin.tie(NULL);
+cin>>t;
+while(t--)
+{
+solution();
+cout<<endl;
+}
+return 0;
+}
