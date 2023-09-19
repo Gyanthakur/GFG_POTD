9002b77d241f68b5b67bf20a567ecdf651307ff9
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-09-19 15:30:47 +0530

    19_september_2k23

---
+++ september_2023/potd_19_09_2023.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+#include<map>
+#include<math.h>
+#include<set>
+using namespace std;
+#define int long long
+long long  t;
+class Solution
+{
+    public:
+    //Function to find position of first set bit in the given number.
+    unsigned int getFirstSetBit(int n)
+    {
+
+        int pos=1;
+      while(n){
+          int x=n%2;
+          if(x==1) return pos;
+          n/=2;
+          pos++;
+      }
+      return 0;
+    }
+};
+void solution()
+{
+
+}
+signed main()
+{
+ios_base::sync_with_stdio(false); 
+cin.tie(NULL);
+cout.tie(NULL);
+cin>>t;
+while(t--)
+{
+solution();
+cout<<endl;
+}
+return 0;
+}
