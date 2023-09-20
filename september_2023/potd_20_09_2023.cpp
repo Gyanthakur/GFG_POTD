46a4b99f21276ae41e709120eef907f03b3c58aa
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-09-20 19:40:09 +0530

    20_september_2k23

---
+++ september_2023/potd_20_09_2023.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+#include<map>
+#include<math.h>
+#include<set>
+using namespace std;
+#define int long long
+long long  t;
+class Solution
+{
+  public:
+        vector <int> rotate (int n, int d)
+        {
+            //code here.
+            d=d%16;
+            int l=(n<<d)|(n>>(16-d));
+            int r=(n>>d)|(n<<(16-d));
+            int mask=(1<<16)-1;
+            l&=mask;    //taking only the first 16 bits
+            r&=mask;    //taking only the first 16 bits
+            return {l,r};
+        }
+};
+void solution()
+{
+
+}
+signed main()
+{
+ios_base::sync_with_stdio(false); 
+cin.tie(NULL);
+cout.tie(NULL);
+cin>>t;
+while(t--)
+{
+solution();
+cout<<endl;
+}
+return 0;
+}
