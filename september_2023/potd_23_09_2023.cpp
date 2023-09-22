6060ec8510f9b9ffe3468d26fa1c5c2d885d0ee6
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-09-23 00:25:36 +0530

    23_september_2k23

---
+++ september_2023/potd_23_09_2023.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <string>
+#include <map>
+#include <math.h>
+#include <set>
+using namespace std;
+#define int long long
+long long t;
+class Solution
+{
+public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n)
+    {
+
+        long long sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += a[i];
+        }
+        long long prev_sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (prev_sum == sum - a[i])
+            {
+                return i + 1;
+            }
+            prev_sum += a[i];
+            sum -= a[i];
+        }
+        return -1;
+    }
+};
+void solution()
+
+{
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> t;
+    while (t--)
+    {
+        solution();
+        cout << endl;
+    }
+    return 0;
+}
