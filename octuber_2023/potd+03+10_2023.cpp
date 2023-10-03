515eff2ca61c44a85f828dce754b51ba191b1399
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-10-03 18:30:41 +0530

    02_octuber_2k23

---
+++ octuber_2023/potd+03+10_2023.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <string>
+#include <map>
+#include <math.h>
+#include <set>
+using namespace std;
+// #define int long long
+long long t;
+class Solution
+{
+public:
+    string colName(long long int n)
+    {
+        // your code here
+        if (n == 0)
+            return "";
+        return colName((n - 1) / 26) + char(65 + (n - 1) % 26);
+    }
+};
+void solution()
+{
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> t;
+    while (t--)
+    {
+        solution();
+        cout << endl;
+    }
+    return 0;
+}
