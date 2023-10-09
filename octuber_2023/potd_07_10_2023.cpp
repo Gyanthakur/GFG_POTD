f9a27de7a0b84c8dabd7da3b6ccddd6aeef04b7d
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-10-09 19:33:02 +0530

    09_octuber_2k23

---
+++ octuber_2023/potd_07_10_2023.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+#include<map>
+#include<math.h>
+#include<set>
+using namespace std;
+#define int long long
+long long  t;
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+}*head;
+
+class Solution
+{
+    public:
+    Node* pairWiseSwap(struct Node* head) 
+    {
+        if(!head || !head->next)
+            return head;
+            
+        Node* iter = head;
+        Node* sec = head->next;
+        Node* prev = NULL;
+        
+        while(iter && iter->next)
+        {
+            Node* second = iter->next;
+            
+            iter->next = second->next;
+            second->next = iter;
+            
+            if(prev)
+                prev->next = second;
+                
+            prev = iter;
+            iter = iter->next;
+        }
+        
+        return sec;
+    }
+};
+void solution()
+{
+
+}
+signed main()
+{
+ios_base::sync_with_stdio(false); 
+cin.tie(NULL);
+cout.tie(NULL);
+cin>>t;
+while(t--)
+{
+solution();
+cout<<endl;
+}
+return 0;
+}
---
+++ octuber_2023/potd_09_10_2023.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+#include<map>
+#include<math.h>
+#include<set>
+using namespace std;
+#define int long long
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+class Solution{
+    public:
+    //Function to find the height of a binary tree.
+    int height(struct Node* node){
+        if(node == NULL)
+            return 0;
+        
+        int leftAns = height(node->left );
+        int rightAns = height(node->right);
+        int ans = max(leftAns , rightAns) + 1;
+        return ans ;
+    }
+};
+long long  t;
+void solution()
+{
+
+}
+signed main()
+{
+ios_base::sync_with_stdio(false); 
+cin.tie(NULL);
+cout.tie(NULL);
+cin>>t;
+while(t--)
+{
+solution();
+cout<<endl;
+}
+return 0;
+}
