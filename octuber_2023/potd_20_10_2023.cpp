6b54db62b06b667614230584d754c94ba95b550d
Jd <jagdishsuthar4413@gmail.com>
2023-10-24 16:32:03 +0530

    codes for 18 to 21 oct

---
+++ octuber_2023/potd_20_10_2023.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int isPossible(int n, int arr[]) {
+        int sum = 0;
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] %= 3;
+            sum += arr[i];
+        }
+        return sum % 3 == 0;
+    }
+};
---
+++ octuber_2023/potd_21_10_2023.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    long long sumOfDivisors(int n)
+    {
+        // Write Your Code here
+        long long sum = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            sum += i*(n/i);
+        }
+        return sum;
+    }
+};
