eb50707a48e62eacb9b3244707c26f8da9fb3837
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-10-28 08:27:06 +0530

    Update potd_28_10_2023.cpp

--- octuber_2023/potd_28_10_2023.cpp
+++ octuber_2023/potd_28_10_2023.cpp
@@ -7,20 +7,64 @@
 using namespace std;
 #define int long long
 long long  t;
-void solution()
-{
 
-}
-signed main()
+class Solution
 {
-ios_base::sync_with_stdio(false); 
-cin.tie(NULL);
-cout.tie(NULL);
-cin>>t;
-while(t--)
-{
-solution();
-cout<<endl;
+public:
+	int is_bleak(int n)
+	{
+	    // Code here.
+	    // 28.10.23 POTD
+	    
+	    int x = n;
+	    
+	    while(x >= (n-log2(n))){
+	        if((x + __builtin_popcount(x)) == n)
+	            return 0;
+	           
+	        x--;
+	    }
+	    
+	    return 1;
+	    
+	    /*  
+	        '__builtin_popcount(x)' returns the number of set bits in the
+	            binary representation of the integer 'x'.
+	    
+	    */
+	}
+};
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	Solution ob;
+    	int ans = ob.is_bleak(n);
+    	cout << ans << "\n";
+    }
+	return 0;
 }
-return 0;
-}
+
+
+// DEFAULT BOILERPLATE
+// void solution()
+// {
+
+// }
+// signed main()
+// {
+// ios_base::sync_with_stdio(false); 
+// cin.tie(NULL);
+// cout.tie(NULL);
+// cin>>t;
+// while(t--)
+// {
+// solution();
+// cout<<endl;
+// }
+// return 0;
+// }
