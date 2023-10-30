abe22cbb26a6cc45469a9b34723f2831c3d83c80
Thrivikramteja <147156748+Thrivikramteja@users.noreply.github.com>
2023-10-30 18:39:05 +0530

    Added Bubble Sort Algorithm
    Implemented Bubble Sort Algorithm using C++

---
+++ bubblesort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+void bubbleSort(int arr[], int n) {
+
+    bool swapped;
+    
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    bubbleSort(arr, n);
+
+    std::cout << "\nSorted array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
