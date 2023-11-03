0b81f510452a9969cdfe343d253978e9df66ab40
Gyanthakur <gps.96169@gmail.com>
2023-11-03 16:40:25 +0530

    03_november_2k23
    Co-Authored-By: ds06 <128789248+DhruvaPratapSingh@users.noreply.github.com>

--- November_2023/potd_03_11_2023.cpp
+++ November_2023/potd_03_11_2023.cpp
@@ -7,6 +7,28 @@
 using namespace std;
 #define int long long
 long long  t;
+class Solution{
+public:
+	// Function to check if the
+	// Pythagorean triplet exists or not
+	bool checkTriplet(int arr[], int n) {
+	    // code here
+	    unordered_map<long long, bool> mp;
+	    for(int i=0;i<n;i++){
+	        mp[arr[i]*arr[i]]= true;
+	    }
+	    
+	    for(int i=0;i<n;i++){
+	        for(int j=i+1;j<n;j++){
+	           long long sum= arr[i]* arr[i] +arr[j]*arr[j];
+	           if(mp[sum]){
+	               return true;
+	           }
+	        }
+	    }
+	    return false;
+	}
+};
 void solution()
 {
 
