b1a972f418018d75aa1579ddc81bcd1f3441e8d2
Gyanthakur <gps.96169@gmail.com>
2023-11-04 22:20:50 +0530

    04_november_2023

--- November_2023/potd_04_11_2023.cpp
+++ November_2023/potd_04_11_2023.cpp
@@ -7,6 +7,14 @@
 using namespace std;
 #define int long long
 long long  t;
+class Solution{
+public:    
+    int transitionPoint(int arr[], int n) {
+        for(int i=0; i<n ;i++)
+            if(arr[i]==1) return i;
+        return -1;
+    }
+};
 void solution()
 {
 
