2f809e84fc4a877b6cbccd78faa2d47a7073c00e
Gyanthakur <gps.96169@gmail.com>
2023-11-13 17:09:56 +0530

    13_november_2k23
    Co-Authored-By: Satyam Singh <111181853+Satyam-Singh-01@users.noreply.github.com>

---
+++ November_2023/potd_13_11_2023.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+#include<map>
+#include<math.h>
+#include<set>
+using namespace std;
+#define int long long
+long long  t;
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    // Function to find length of the shortest common supersequence of two strings.
+    int shortestCommonSupersequence(string X, string Y, int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        // Fill the dp table using bottom-up approach.
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = i + j;
+                else if (X[i - 1] == Y[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[m][n];
+    }
+};
+void solution()
+{
+
+}
+signed main()
+{
+ios_base::sync_with_stdio(false); 
+cin.tie(NULL);
+cout.tie(NULL);
+cin>>t;
+while(t--)
+{
+solution();
+cout<<endl;
+}
+return 0;
+}
