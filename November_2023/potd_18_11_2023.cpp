24645f519b3ed4cee5cb6f149a8b7a8c08fd5e12
Gyanthakur <gps.96169@gmail.com>
2023-11-18 18:18:02 +0530

    18_november_2k23
    Co-Authored-By: RISHIKA <120507340+rishika-g31@users.noreply.github.com>

---
+++ November_2023/potd_18_11_2023.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+#include<map>
+#include<math.h>
+#include<set>
+using namespace std;
+#define int long long
+long long  t;
+class Solution
+{
+    public:
+    Node* reverseDLL(Node * head)
+    {
+        //Your code here
+        
+        Node *current = head;
+        Node *temp = NULL;
+        
+        while (current != NULL)
+        {
+            temp = current->prev;
+            current->prev = current->next;
+            current->next = temp;
+            current = current->prev;
+        }
+        
+        if (temp != NULL)
+            head = temp->prev;
+        
+        return head;
+        
+    }
+};
+void solution()
+{
+
+}
+signed main()
+{
+ios_base::sync_with_stdio(false); 
+cin.tie(NULL);
+cout.tie(NULL);
+cin>>t;
+while(t--)
+{
+solution();
+cout<<endl;
+}
+return 0;
+}
