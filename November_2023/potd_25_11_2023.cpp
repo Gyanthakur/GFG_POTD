1b6b67c2dc02eab1d17cf73c10e0a40d67fd1969
Gyanthakur <gps.96169@gmail.com>
2023-11-25 18:18:56 +0530

    potd_25_11_2023
    Co-Authored-By: RISHIKA <120507340+rishika-g31@users.noreply.github.com>

---
+++ November_2023/potd_25_11_2023.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+#include<map>
+#include<math.h>
+#include<set>
+using namespace std;
+#define int long long
+long long  t;
+class Solution{
+public:
+void shuffleArray(int arr[],int n)
+{
+    // Your code goes here
+    int aindex=0;
+    int bindex=n/2;
+    int maxi= *max_element(arr,arr+n)+1;
+    for(int i=0;i<n;i++){
+            arr[i]=arr[i]*maxi;
+    }
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+            arr[i]+=arr[aindex]/maxi;
+            aindex++;
+        }
+        else{
+            arr[i]+=arr[bindex]/maxi;
+            bindex++;
+        }
+    }
+    for(int i=0;i<n;i++){
+            arr[i]=arr[i]%maxi;
+    }
+    
+} 
+};
+
+void solution()
+{
+
+}
+signed main()
+{
+ios_base::sync_with_stdio(false); 
+cin.tie(NULL);
+cout.tie(NULL);
+cin>>t;
+while(t--)
+{
+solution();
+cout<<endl;
+}
+return 0;
+}
