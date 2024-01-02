b7fabb474835f2036ba54d1c683a02575c7233e7
Gyanthakur <gps.96169@gmail.com>
2024-01-02 17:03:13 +0530

    potd_02_01_2024
    Co-Authored-By: RISHIKA <120507340+rishika-g31@users.noreply.github.com>

---
+++ 2024/january/potd_02_01)2024.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+#include<map>
+#include<math.h>
+#include<set>
+using namespace std;
+#define int long long
+long long  t;
+class Solution{
+    public:    
+    long long int maxSumWithK(long long int a[], long long int n, long long int k) 
+    {
+        long long int sum = 0, sum2 = 0, ans = LLONG_MIN;
+        int cnt = 0;
+        
+        for(int i = 0, j = 0; i < n; ++i) {
+            sum += a[i];
+            if(i-j+1>=k) {
+                sum -= a[j];
+                if(sum2 < 0) sum2 = 0;
+                sum2 += a[j++];
+                ans = max(ans, sum + sum2);
+            }
+        }
+        
+        return ans;
+    }
+};
+void solution()
+{
+
+}
+signed main()
+{
+ios_base::sync_with_stdio(false); 
+cin.tie(NULL);
+cout.tie(NULL);
+cin>>t;
+while(t--)
+{
+solution();
+cout<<endl;
+}
+return 0;
+}
