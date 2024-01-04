a927278f253953bdd353ceb8cee790838636f1a6
Gyanthakur <gps.96169@gmail.com>
2024-01-04 09:07:10 +0530

    potd_04_01_2023
    Co-Authored-By: RISHIKA <120507340+rishika-g31@users.noreply.github.com>

---
+++ 2024/january/potd_04_01_2023.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+#include<map>
+#include<math.h>
+#include<set>
+using namespace std;
+#define int long long
+long long  t;
+class Solution {
+  public:
+    int singleElement(int arr[] ,int n) {
+        // code here
+        sort(arr,arr+n);
+        int ans;
+        map<int,int>m;
+        for(int i = 0;i<n;i++)
+        {
+            m[arr[i]]++;
+        }
+        for(auto it:m)
+        {
+            if(it.second == 1)
+            ans = it.first;
+        }
+        return ans;
+    }
+};
+void solution()
+{
+
+}
+signed main()
+{
+ios_base::sync_with_stdio(false); 
+cin.tie(NULL);
+cout.tie(NULL);
+cin>>t;
+while(t--)
+{
+solution();
+cout<<endl;
+}
+return 0;
+}
