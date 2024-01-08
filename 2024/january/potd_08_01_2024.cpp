f0672d290efc519bc4c65ac6d234a5718215cee7
Gyanthakur <gps.96169@gmail.com>
2024-01-08 10:23:01 +0530

    potd_08_01_2024

---
+++ 2024/january/potd_08_01_2024.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+#include<map>
+#include<math.h>
+#include<set>
+using namespace std;
+#define int long long
+long long  t;
+string encode(string src)
+{     
+  //Your code here 
+  int length = src.length();
+  int count=0;
+  int i=0;
+  string ans="";
+
+  while(i<length){
+      char a=src[i];
+      ans+=a;
+      while(src[i]==a){
+          count++;
+          i++;
+      }
+      ans+=to_string(count);
+      count=0;
+      
+  }
+  return ans;
+  
+}     
+void solution()
+{
+
+}
+signed main()
+{
+ios_base::sync_with_stdio(false); 
+cin.tie(NULL);
+cout.tie(NULL);
+cin>>t;
+while(t--)
+{
+solution();
+cout<<endl;
+}
+return 0;
+}
