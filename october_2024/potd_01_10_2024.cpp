95f1e0c0de80fcc9f12f3b924354026a155b4144
Syeda Fariha Naqvi <71366962+farihaNaqvi@users.noreply.github.com>
2024-10-01 20:35:56 +0530

    Create potd_01_10_2024.cpp
    gfg potd was Multiply two linked lists

---
+++ october_2024/potd_01_10_2024.cpp
@@ -0,0 +1,40 @@
+/* Linked list node structure
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};*/
+
+/*The method multiplies
+two  linked lists l1 and l2
+and returns their product*/
+
+/* Multiply contents of two linked lists */
+class solution {
+  public:
+    long long multiplyTwoLists(Node *first, Node *second) {
+        long long num1=0,num2=0;
+        long long MOD = 1000000007;
+        Node* cur1=first;
+        Node* cur2=second;
+        while(cur1 != nullptr)
+        {
+            num1=num1*10+cur1->data;
+            cur1=cur1->next;
+        }
+        while(cur2 != nullptr)
+        {
+            num2 = num2*10 + cur2->data;
+            cur2=cur2->next;
+        }
+        
+        return (num1*num2)%MOD;
+        
+    }
+};
