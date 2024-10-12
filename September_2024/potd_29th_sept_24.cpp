2cf49ab2c84f81cdd75867feee8f10dcda9aa529
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-10-12 20:00:34 +0530

    Create potd_29th_sept_24.cpp

---
+++ September_2024/potd_29th_sept_24.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int totalCount(int k, vector<int>& arr) {
+        int total = 0;
+        for(auto &x : arr){
+            total += (x + k - 1) / k;
+        }
+        return total;
+    }
+};
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        string ks;
+        getline(cin, ks);
+        while(ks.empty()){
+            getline(cin, ks);
+        }
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        while(input.empty()){
+            getline(cin, input);
+        }
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.totalCount(k, arr);
+        cout << res << endl;
+    }
+    return 0;
+}
