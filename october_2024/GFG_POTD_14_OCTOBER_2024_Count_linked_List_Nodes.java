9e18c741783bc1be2968d4be0aec7a57119a86eb
Samruddhi Adhikary <136476753+Samruddhi345A@users.noreply.github.com>
2024-10-14 00:41:34 +0530

    GFG POTD 14 october 2024 in java
    issue: #8
    Problem : Count Linked List Nodes
    Date: 14th October 2024

---
+++ october_2024/GFG_POTD_14_OCTOBER_2024_Count_linked_List_Nodes.java
@@ -0,0 +1,20 @@
+
+class Solution {
+     Function to count nodes of a linked list.
+    public int getCount(Node head) {
+         code here
+        int count=0;
+        
+        if(head==null){
+            count=0;
+        }
+        else{
+            Node ptr=head;
+            while(ptr!=null){
+                count++;
+                ptr=ptr.next;
+            }
+        }
+        return count;
+    }
+}
