0f7b4244a4d72538eb9b75969fd2a96ca68c6f4d
Jagmohan Gautam <jagmohangautam7@gmail.com>
2024-10-25 21:59:23 +0530

    added solution for GFG_POTD_25-10-2024_Alternative_Sorting

---
+++ october_2024/GFG_POTD_25-10-2024_Alternative_Sorting.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    vector<int> alternateSort(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        vector<int>ans;
+        int i=0,j=n-1;
+        while(i<j)
+        {
+            ans.push_back(arr[j--]);
+            ans.push_back(arr[i++]);
+        }
+        if(n%2!=0)
+        ans.push_back(arr[i]);
+        return ans;
+    }
+};
