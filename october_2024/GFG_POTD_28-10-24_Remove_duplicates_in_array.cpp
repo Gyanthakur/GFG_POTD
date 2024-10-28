3278e80c2d6a1470d496470f777fed484356f9d5
Jagmohan Gautam <jagmohangautam7@gmail.com>
2024-10-28 22:56:48 +0530

    solved GFG_POTD_28-10-2024 Remove duplicates in array

---
+++ october_2024/GFG_POTD_28-10-24_Remove_duplicates_in_array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    vector<int> removeDuplicate(vector<int>& arr) {
+        vector<int>ans;
+        unordered_set<int>st;
+        for(int &it:arr){
+            if(st.find(it)==st.end()){
+                ans.push_back(it);
+                st.insert(it);
+            }
+        }
+        return ans;
+    }
+};
