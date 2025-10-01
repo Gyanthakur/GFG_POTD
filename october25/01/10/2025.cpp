63c500d38759dfb4f311713d6af1ca5395b5e60c
Gyanthakur <gps.96169@gmail.com>
2025-10-01 12:05:57 +0530

    1 October 25

---
+++ october25/01/10/2025.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    vector<vector<int>> uniquePerms(vector<int>& arr) {
+        // code here
+        vector<vector<int>>ans;
+        sort(arr.begin(), arr.end());
+        ans.push_back(arr);
+        while(next_permutation(arr.begin() , arr.end())) ans.push_back(arr); //next_permutaion is an funtion that creates the permutation automatically in the greater lexicographic way.
+        return ans;
+    }
+};
