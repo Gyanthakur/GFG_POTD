6c61ebd021216490b3f4b6b3c1c51aa34f554f8f
Barun Ojha <barunojha301@gmail.com>
2025-10-05 17:04:22 +0530

    Added cpp version of potd_02_10_2025
    This is a cpp solution for the GFG problem of the day 02_10_2025.

---
+++ October_2025/potd_02_10_2025.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> current;
+        backtrack(1, current, 0, k, n, result);
+        return result;
+    }
+    
+private:
+    void backtrack(int start, vector<int>& current, int sum, int k, int n, vector<vector<int>>& result) {
+        if (current.size() == k) {
+            if (sum == n) result.push_back(current);
+            return;
+        }
+        
+        for (int i = start; i <= 9; i++) {
+            if (sum + i > n) break;
+            current.push_back(i);
+            backtrack(i + 1, current, sum + i, k, n, result);
+            current.pop_back();
+        }
+    }
+};
