dcfb404881c5fc0f47a77e0b00242fdf931dd0a8
Roshan Kumar Singh <155691968+Roshansingh9@users.noreply.github.com>
2025-10-05 02:52:11 +0000

    added myself

---
+++ October_2025/potd_05_10_2025.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<string> ratInMaze(vector<vector<int>>& maze) {
+        int n = maze.size();
+        vector<string> paths;
+        vector<vector<int>> visited(n, vector<int>(n, 0));
+        string path = "";
+
+        function<void(int,int)> dfs = [&](int x, int y) {
+            if (x < 0 || y < 0 || x >= n || y >= n || visited[x][y] || maze[x][y] == 0) return;
+            if (x == n - 1 && y == n - 1) {
+                paths.push_back(path);
+                return;
+            }
+            
+            visited[x][y] = 1;
+
+            path.push_back('D');
+            dfs(x + 1, y);
+            path.pop_back();
+
+            path.push_back('L');
+            dfs(x, y - 1);
+            path.pop_back();
+
+            path.push_back('R');
+            dfs(x, y + 1);
+            path.pop_back();
+
+            path.push_back('U');
+            dfs(x - 1, y);
+            path.pop_back();
+
+            visited[x][y] = 0;
+        };
+
+        dfs(0, 0);
+        sort(paths.begin(), paths.end());
+        return paths;
+    }
+};
